60128140e6da107b921f3a6e0ef6c6493c8a05d3
rortegaez <raulor83@gmail.com>
2022-03-08 18:58:58 +0100

    final v.15 addEventListener()

--- js/JavaVideo12.js
+++ js/JavaVideo12.js
@@ -4,9 +4,28 @@ function ejecuta(){
    /* for(var i=0; i<3; i++){
    document.getElementsByTagName("p")[i].onclick=saludo;
    document.getElementById("importante").onclick=saludo;
-    }*/
-    var z=document.getElementsByClassName("importante")[0].onclick=saludo;
+    }
+    var z=document.getElementsByClassName("importante")[0].onclick=saludo;*/
 
+    /*document.querySelector(".importante").onclick=saludo;*/
+    /* onclick=evento
+    saludo=función
+    document. =estamos nombrando al documento linkado
+    querySelector() = estamos diciendo el primer elemento que coincida con el selector*/
+
+    /*var elementos=document.querySelectorAll("#principal p")[0].onclick=saludo;*/
+    /*esta última función es para seleccionar el primer elemento #principal p*/
+
+    /*for(var i=0;i<4;i++){
+        var elementos=document.querySelectorAll("#principal p");
+        elementos[i].onclick=saludo;
+    }*/
+
+    var elementos=document.querySelectorAll("#principal p");
+    for(var i=0;i<document.length;i++){
+        elementos[i].onclick=saludo;
+    }
+    /* "i<document.length" nos esta indicando que i debe de ser menor que el tamaño de document*/
 }
 
 function saludo (){
@@ -14,3 +33,4 @@ alert('Que hay de nuevo?');
 }
 
 window.onload=ejecuta;
+s
